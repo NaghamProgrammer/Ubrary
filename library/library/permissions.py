08b86e949c06a0dcc0d35d3f2909f3444533d4d2
Nagham Wael <161651186+NaghamProgrammer@users.noreply.github.com>
2025-06-03 22:49:41 +0300

    Delete library directory

--- library/library/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsCustomAdmin(BasePermission):
-    """
-    Allows access only to users with is_admin=True.
-    """
-    def has_permission(self, request, view):
-        return request.user and request.user.is_authenticated and request.user.is_admin
